59aad5a4d23214b2f0621c1d18288b3ac47404e8
Sergey Kustarnikov <sergei.03.k@gmail.com>
2020-10-03 01:30:12 +0300

    feat: solved typical-arrays-pronlems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
 exports.min = function min (array) {
+  if (!array || array.length === 0) 
   return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
+  if (!array || array.length === 0)
   return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
+  if (!array || array.length === 0) 
   return 0;
+  return array.reduce((a,b) => a + b, 0) / array.length;
+  
 }
